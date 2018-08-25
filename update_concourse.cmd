f7116f42cd0be2bad4e8ba5adb536efa26452d27
Jeff DeCola (TK1-PC GIT FOR WINDOWS) <jeff@keeperlabs.com>
2018-08-25 00:56:24 -0700

    set log level to error in concourse docker containers

--- update_concourse.cmd
+++ update_concourse.cmd
@@ -2,9 +2,9 @@ REM my-cheat-sheets update_concourse.cmd
 @ECHO OFF
 ECHO "START update_concourse.cmd script"
 ECHO.
-REM Assumed you ran "C:\Program Files (x86)\Concourse\fly.exe" -t ci login -c http://192.168.100.4:8080/
+REM Assumed you ran "C:\Program Files (x86)\Concourse\fly-4.0.0.exe" -t ci login -c http://192.168.100.6:8080/
 ECHO "Run Fly to update my-cheat-sheets pipeline" 
 ECHO.
-REM "C:\Program Files (x86)\Concourse\fly.exe" -v
-START "concourse" "C:\Program Files (x86)\Concourse\fly.exe" -t ci set-pipeline -p my-cheat-sheets -c ci/pipeline.yml --load-vars-from ci/.credentials.yml
+REM "C:\Program Files (x86)\Concourse\fly-4.0.0.exe" -v
+START "concourse" "C:\Program Files (x86)\Concourse\fly-4.0.0.exe" -t ci set-pipeline -p my-cheat-sheets -c ci/pipeline.yml --load-vars-from ci/.credentials.yml
 ECHO "END update_concourse.cmd script"
