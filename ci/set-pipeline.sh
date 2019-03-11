5d64a125dd21390d9814824c4b8b06829cbdda51
Jeff DeCola (TK1-PC GIT FOR WINDOWS) <jeff@keeperlabs.com>
2019-03-11 10:03:55 -0700

    moved concourse files to ci area

---
+++ ci/set-pipeline.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# my-cheat-sheets set-pipeline.sh
+
+fly -t ci set-pipeline -p my-cheat-sheets -c pipeline.yml --load-vars-from ../../../../../.credentials.yml
--- update_concourse.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# my-cheat-sheets update_concourse.sh
-
-fly -t ci set-pipeline -p my-cheat-sheets -c ci/pipeline.yml --load-vars-from ../../.credentials.yml
